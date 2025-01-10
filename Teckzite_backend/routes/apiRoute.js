e4624183d97eb62409bdb13a12781f90e3405cbf
Chintalapudi Chandra Sekhar <chanduchintalapudi9@gmail.com>
2025-01-10 22:32:58 +0530

    made some corrections

--- Teckzite_backend/routes/apiRoute.js
+++ Teckzite_backend/routes/apiRoute.js
@@ -5,7 +5,7 @@ const authenticateAdmin = require('../middleware/auth');
 router.get('/players',apiController.getplayers);
 router.get('/playersToBuy',apiController.playersToBuy);
 router.get('/soldPlayers',apiController.soldPlayers);
-router.get('getTeams',apiController.getTeams);
+router.get('/getTeams',apiController.getTeams);
 
 router.post('/player',authenticateAdmin,apiController.player);
 router.post("/createTeam",authenticateAdmin,apiController.createTeam);

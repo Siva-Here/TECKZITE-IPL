711b16fde17e92a15df8faf5221cf0c89fb68e9e
Chintalapudi Chandra Sekhar <chanduchintalapudi9@gmail.com>
2025-01-29 22:20:01 +0530

    Installing tailwind css

--- Teckzite_admin/src/main.jsx
+++ Teckzite_admin/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import './index.css'; 
 import { AuthProvider } from './Authcontext';
 import IPLTeamCard from './components/TeamCard.jsx';
 // import Profilecard from './components/Profilecard.jsx';
